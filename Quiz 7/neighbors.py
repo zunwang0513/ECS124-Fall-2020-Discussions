5ca5f20084e0ba30418d168ba57f32c6a2749bbe
Parsoa Khorsand <parsoakhorsand91@gmail.com>
2020-11-28 03:04:23 -0800

    Add dummy example for Quiz 7.

---
+++ Quiz 7/neighbors.py
@@ -0,0 +1,12 @@
+
+def main(n, d):
+    pass
+
+# this file must only include function definitions
+# comment out code you may have for testing before submitting your code:
+#with open('rosalind.txt') as f:
+#    n = int(f.readline())
+#    d = []
+#    for i in range(n):
+#        d.append([int(t) for t in f.readline().split()])
+#t = main(n, d)
